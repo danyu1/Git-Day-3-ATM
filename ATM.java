86083e45a92bccdf882aa53047049be04ee6aaad
danyul <danieltonyh06@gmail.com>
2023-08-30 12:57:07 -0700

    added the openAccount method

--- ATM.java
+++ ATM.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
 import java.util.HashMap;
 
 public class ATM {
+    private HashMap<String, Double> accounts;
 
+    public ATM() {
+        this.accounts = new HashMap<String, Double>();
+    }
+
+    public void openAccount(String email, Double initialDeposit) throws IOException {
+        if (!accounts.containsKey(email)) {
+            accounts.put(email, initialDeposit);
+        } else {
+            throw new IOException("This email is already associated with an account.");
+        }
+    }
 }
